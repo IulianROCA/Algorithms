d460d5456c9e7f1a525153a95cc69419d469a276
Iulian ROCA <rocaiulian@gmail.com>
2024-04-17 16:26:43 +0300

    recommited pattern algorithm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,18 @@
 package org.example;
 
-import org.example.armstrongnumber.ArmstrongNumber;
-import org.example.factorialnumber.FactorialNumber;
-import org.example.fibonacci.FibonacciSeries;
-import org.example.matrixmultiplication.Matrix;
-import org.example.polindrom.Polindrom;
-import org.example.primenumber.PrimeNumber;
-import org.example.stringreverse.StringReverse;
+import org.example.exercises.Exercises;
 
 public class Main {
     public static void main(String[] args) {
         //new StringReverse().stringReverse();
         //new FibonacciSeries().fibonacciSeries();
         //new FactorialNumber().factorialNumber();
-        //new Polindrom().polindrom();
+        //new Palindrome().palindrome();
         //new PrimeNumber().isPrimeNumber();
         //new ArmstrongNumber().isArmstrongNumber();
-        new Matrix().multiplication();
+        //new Matrix().multiplication();
+        //new PatternsRightTriangle().displayPattern();
+        //new PatternsIsoscelesTriangle().displayIsoscelesTriangle();
+        new Exercises().convertIntToBinary(5);
     }
 }
